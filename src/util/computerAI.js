257792ae064ffa9117a75f98ac9d00d0b7a59ec4
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-10-31 08:51:13 +0000

    added harder ai

--- src/util/computerAI.js
+++ src/util/computerAI.js
@@ -24,13 +24,40 @@ export const generateAIHard = (tiles) => {
 		// for each player, test it against every empty tile and check if it is a winning move.
 		// if any winning move, player or AI, the AI will want to play in that tile
 		for (const index of emptyTiles) {
-			boardCopy = [...tiles];
+			const boardCopy = [...tiles];
 			boardCopy[index] = player;
-			if (checkWinner(boardCopy)) return index;
+			if (checkWinner(boardCopy, () => {})) return index;
 		}
 	}
 
 	// check for open corner
+	let openCorners = [];
+	for (let index of emptyTiles) {
+		if ([0, 2, 6, 8].includes(index)) {
+			openCorners.push(index);
+		}
+	}
+	if (openCorners.length > 0) {
+		let index = openCorners[Math.floor(Math.random() * openCorners.length)];
+		return index;
+	}
+
+	// check for open center
+	for (let index of emptyTiles) {
+		if ([4].includes(index)) {
+			return index;
+		}
+	}
 
 	// check for open edges
+	let openEdges = [];
+	for (let index of emptyTiles) {
+		if ([1, 3, 5, 7].includes(index)) {
+			openEdges.push(index);
+		}
+	}
+	if (openEdges.length > 0) {
+		let index = openEdges[Math.floor(Math.random() * openEdges.length)];
+		return index;
+	}
 };
