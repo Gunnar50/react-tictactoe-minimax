cbd8e576c2fd6968fcc93731b4198ffd9e9cc304
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-10-31 08:52:14 +0000

    added game stats

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,40 +1,96 @@
 import React, { useEffect, useState } from "react";
-import NEW_GAME from "../assets/images/new-game.png";
 import PLAYER_O from "../assets/images/player-o.png";
 import PLAYER_X from "../assets/images/player-x.png";
+import { checkWinner, isBoardFull } from "../util/checkWinner";
+import { generateAIEasy, generateAIHard } from "../util/computerAI";
+import { GameState } from "../util/gameState";
 import Board from "./Board";
+import GameOver from "./GameOver";
+import GameStats from "./GameStats";
 
 function Game() {
 	const [tiles, setTiles] = useState(Array(9).fill(null));
-	const [playerTurn, setPlayerTurn] = useState(PLAYER_X);
+	const [playerTurn, setPlayerTurn] = useState(Math.floor(Math.random() * 2));
+	const [winnerStrikeClass, setWinnerStrikeClass] = useState("");
+	const [gameState, setGameState] = useState(GameState.inGame);
+	const [gameStats, setGameStats] = useState({
+		player: 0,
+		computer: 0,
+		tie: 0,
+		gamesPlayed: 0,
+	});
+
+	const makeMove = (index) => {
+		const newTiles = [...tiles];
+		newTiles[index] = playerTurn ? PLAYER_X : PLAYER_O;
+		setTiles(newTiles);
+		setPlayerTurn(!playerTurn);
+	};
+
+	const updateStats = (updatedGameState) => {
+		setGameStats((prevStats) => {
+			let newStats = { ...prevStats, gamesPlayed: prevStats.gamesPlayed + 1 };
+
+			if (updatedGameState === GameState.playerWin) {
+				newStats.player = prevStats.player + 1;
+			} else if (updatedGameState === GameState.computerWin) {
+				newStats.computer = prevStats.computer + 1;
+			} else if (updatedGameState === GameState.tie) {
+				newStats.tie = prevStats.tie + 1;
+			}
+
+			newStats.gamesPlayed = prevStats.gamesPlayed + 1;
+
+			return newStats;
+		});
+	};
 
 	useEffect(() => {
-		checkWinner();
+		const winner = checkWinner(tiles, setWinnerStrikeClass);
+		let updatedGameState = gameState;
+
+		if (winner) {
+			updatedGameState =
+				winner === PLAYER_X ? GameState.playerWin : GameState.computerWin;
+			setGameState(updatedGameState);
+			updateStats(updatedGameState);
+			return;
+		}
+		if (isBoardFull(tiles)) {
+			setGameState(GameState.tie);
+			updateStats(GameState.tie);
+			return;
+		}
+		if (!playerTurn) {
+			const computerMove = generateAIHard(tiles);
+			makeMove(computerMove);
+		}
 	}, [tiles]);
 
 	const handleTileClick = (index) => {
-		if (tiles[index] !== null) return;
+		if (gameState !== GameState.inGame || !playerTurn) return; // return if game state is not in game
+		if (tiles[index] !== null) return; // return if tile is not null
 
-		const newTiles = [...tiles];
-		newTiles[index] = playerTurn;
-		setTiles(newTiles);
-
-		if (playerTurn === PLAYER_X) setPlayerTurn(PLAYER_O);
-		else setPlayerTurn(PLAYER_X);
+		makeMove(index);
 	};
 
 	const handleNewGame = () => {
 		setTiles(Array(9).fill(null));
-		setPlayerTurn(PLAYER_X);
+		setPlayerTurn(Math.floor(Math.random() * 2));
+		setGameState(GameState.inGame);
+		setWinnerStrikeClass(null);
 	};
 
 	return (
 		<div className="game">
-			<h1>TicTacToe</h1>
-			<Board tiles={tiles} onTileClick={handleTileClick} />
-			<div className="new-game">
-				<img src={NEW_GAME} onClick={handleNewGame} />
-			</div>
+			<h1>Tic-Tac-Toe</h1>
+			<GameStats gameStats={gameStats} />
+			<Board
+				tiles={tiles}
+				onTileClick={handleTileClick}
+				strikeClass={winnerStrikeClass}
+			/>
+			<GameOver gameState={gameState} handleNewGame={handleNewGame} />
 		</div>
 	);
 }
