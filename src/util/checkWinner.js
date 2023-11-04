9db81f108aeb16f35198785b06d60101702603ff
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-11-04 17:10:03 +0000

    refractor

--- src/util/checkWinner.js
+++ src/util/checkWinner.js
@@ -1,32 +1,32 @@
-export const checkWinner = (tiles, setStrikeClass) => {
-	if (tiles[0] && tiles[0] === tiles[1] && tiles[0] === tiles[2]) {
+export const checkWinner = (board, setStrikeClass) => {
+	if (board[0] && board[0] === board[1] && board[0] === board[2]) {
 		setStrikeClass("strike-row-1");
-		return tiles[0];
-	} else if (tiles[3] && tiles[3] === tiles[4] && tiles[3] === tiles[5]) {
+		return board[0];
+	} else if (board[3] && board[3] === board[4] && board[3] === board[5]) {
 		setStrikeClass("strike-row-2");
-		return tiles[3];
-	} else if (tiles[6] && tiles[6] === tiles[7] && tiles[6] === tiles[8]) {
+		return board[3];
+	} else if (board[6] && board[6] === board[7] && board[6] === board[8]) {
 		setStrikeClass("strike-row-3");
-		return tiles[6];
-	} else if (tiles[0] && tiles[0] === tiles[3] && tiles[0] === tiles[6]) {
+		return board[6];
+	} else if (board[0] && board[0] === board[3] && board[0] === board[6]) {
 		setStrikeClass("strike-column-1");
-		return tiles[0];
-	} else if (tiles[1] && tiles[1] === tiles[4] && tiles[1] === tiles[7]) {
+		return board[0];
+	} else if (board[1] && board[1] === board[4] && board[1] === board[7]) {
 		setStrikeClass("strike-column-2");
-		return tiles[1];
-	} else if (tiles[2] && tiles[2] === tiles[5] && tiles[2] === tiles[8]) {
+		return board[1];
+	} else if (board[2] && board[2] === board[5] && board[2] === board[8]) {
 		setStrikeClass("strike-column-3");
-		return tiles[2];
-	} else if (tiles[0] && tiles[0] === tiles[4] && tiles[0] === tiles[8]) {
+		return board[2];
+	} else if (board[0] && board[0] === board[4] && board[0] === board[8]) {
 		setStrikeClass("strike-diagonal-1");
-		return tiles[0];
-	} else if (tiles[2] && tiles[2] === tiles[4] && tiles[2] === tiles[6]) {
+		return board[0];
+	} else if (board[2] && board[2] === board[4] && board[2] === board[6]) {
 		setStrikeClass("strike-diagonal-2");
-		return tiles[2];
+		return board[2];
 	}
 	return false;
 };
 
-export const isBoardFull = (tiles) => {
-	return tiles.every((tile) => tile !== null);
+export const isBoardFull = (board) => {
+	return board.every((tile) => tile !== null);
 };
